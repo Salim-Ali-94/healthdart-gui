06495f71488724afeb0d4b99cbfcb1f52e85350b
Salim Ali <75537889+Salim-Ali-94@users.noreply.github.com>
2022-06-29 08:29:37 +0200

    Update ConsultScreen.js

--- screens/ConsultScreen.js
+++ screens/ConsultScreen.js
@@ -6,7 +6,7 @@ export default function ConsultScreen() {
 
   return (
 
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ backgroundColor: "#fff", flex: 1, alignItems: "center", justifyContent: "center" }}>
 
       <Text>Consult Screen</Text>
 
